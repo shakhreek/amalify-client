bf9c4d495b10cf6bec1b3671fe09d1acb1f599fc
MuxsinjonAmalify <muxsinjonamalify@gmail.com>
2024-11-15 14:32:58 +0500

    Dashboard updated
    Added filter button in dashboard also changed statuses as well as added done icon to the items

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,158 +1,196 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
-    Card,
-    CardHeader,
-    CardContent,
-    Chip,
-    Paper
-} from '@mui/material';
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  Chip,
+  Paper,
+  Button,
+} from "@mui/material";
+import DoneIcon from "@mui/icons-material/Done";
 
 const latestOrders = {
-    header: [
-        "Customer ID",
-        "Customer",
-        "Phone Number",
-        "Date",
-        "Status", 
-        "Product"
-    ],
-    body: [
-        {
-            id: "#OD1711",
-            user: "john doe",
-            date: "17 Jun 2021",
-            price: "$900",
-            status: "shipping",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1712",
-            user: "frank iva",
-            date: "1 Jun 2021",
-            price: "$400",
-            status: "paid",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "pending",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1712",
-            user: "frank iva",
-            date: "1 Jun 2021",
-            price: "$400",
-            status: "paid",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "refund",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "refund",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "refund",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "refund",
-            product: "Ozone Generator"
-        },
-        {
-            id: "#OD1713",
-            user: "anthony baker",
-            date: "27 Jun 2021",
-            price: "$200",
-            status: "refund",
-            product: "Ozone Generator"
-        },
-    ]
+  header: [
+    "Customer ID",
+    "Customer",
+    "Phone Number",
+    "Date",
+    "Status",
+    "Product",
+  ],
+  body: [
+    {
+      id: "#OD1711",
+      user: "john doe",
+      date: "17 Jun 2021",
+      price: "$900",
+      status: "interested",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "interested",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "interested",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1712",
+      user: "frank iva",
+      date: "1 Jun 2021",
+      price: "$400",
+      status: "potential",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "potential",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "potential",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "contracted",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "contracted",
+      product: "Ozone Generator",
+    },
+    {
+      id: "#OD1713",
+      user: "anthony baker",
+      date: "27 Jun 2021",
+      price: "$200",
+      status: "contracted",
+      product: "Ozone Generator",
+    },
+  ],
 };
 
 const orderStatusColors = {
-    "shipping": "primary",
-    "pending": "warning",
-    "paid": "success",
-    "refund": "error"
+  contracted: "success",
+  interested: "error",
+  potential: "warning",
 };
 
 const Dashboard = () => {
-    const themeReducer = useSelector(state => state.ThemeReducer.mode);
+  const themeReducer = useSelector((state) => state.ThemeReducer.mode);
 
-    return (
-        <div>
-            <Typography variant="h4" component="h2" gutterBottom>
-                Dashboard
-            </Typography>
-            <div>
-                <Card>
-                    <CardHeader title="Call Duties" />
-                    <CardContent>
-                        <TableContainer component={Paper} sx={{ maxHeight: 500, backgroundColor: 'white' }}>
-                            <Table stickyHeader aria-label="latest orders table" sx={{ backgroundColor: 'white' }}>
-                                <TableHead>
-                                    <TableRow>
-                                        {latestOrders.header.map((item, index) => (
-                                            <TableCell key={index} sx={{ fontWeight: 'bold' }}>{item}</TableCell>
-                                        ))}
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {latestOrders.body.map((item, index) => (
-                                        <TableRow key={index}>
-                                            <TableCell>{item.id}</TableCell>
-                                            <TableCell>{item.user}</TableCell>
-                                            <TableCell>{item.price}</TableCell>
-                                            <TableCell>{item.date}</TableCell>
-                                            <TableCell>
-                                                <Chip
-                                                    label={item.status}
-                                                    color={orderStatusColors[item.status]}
-                                                    variant="outlined"
-                                                />
-                                            </TableCell>
-                                            <TableCell>{item.product}</TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </CardContent>
-                </Card>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Typography variant="h4" component="h2" gutterBottom>
+        Dashboard
+      </Typography>
+      <div>
+        <Card>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              maxWidth: "90%",
+            }}
+          >
+            <CardHeader title="Call Duties" />
+            <Button variant="contained">Filter</Button>
+          </div>
+          <CardContent>
+            <TableContainer
+              component={Paper}
+              sx={{ maxHeight: 500, backgroundColor: "white" }}
+            >
+              <Table
+                stickyHeader
+                aria-label="latest orders table"
+                sx={{ backgroundColor: "white" }}
+              >
+                <TableHead>
+                  <TableRow>
+                    {latestOrders.header.map((item, index) => (
+                      <TableCell key={index} sx={{ fontWeight: "bold" }}>
+                        {item}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {latestOrders.body.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{item.id}</TableCell>
+                      <TableCell>{item.user}</TableCell>
+                      <TableCell>{item.price}</TableCell>
+                      <TableCell>{item.date}</TableCell>
+                      <TableCell>
+                        <Chip
+                          label={item.status}
+                          color={orderStatusColors[item.status] || "default"}
+                          variant="outlined"
+                        />
+                      </TableCell>
+                      <TableCell
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "space-between",
+                          paddingTop: 25,
+                        }}
+                      >
+                        {item.product}
+                        <DoneIcon
+                          sx={{
+                            cursor: "pointer",
+                            "&:hover": {
+                              color: "blue", // Hover bo'lganda rangni o'zgartirish
+                              transform: "scale(1.1)", // Kichik animatsiya
+                            },
+                          }}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;

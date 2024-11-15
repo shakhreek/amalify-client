7fc4fe3feef656251083f2e198f9026ab32184b0
MuxsinjonAmalify <muxsinjonamalify@gmail.com>
2024-11-16 00:31:57 +0500

    Login page conflicts fixed

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { Route, Switch } from "react-router-dom";
-
 import Dashboard from "../pages/Dashboard";
 import Customers from "../pages/Customers";
 import Leads from "../pages/Leads";
